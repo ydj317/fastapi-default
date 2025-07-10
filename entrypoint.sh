49d9ebe510bd38742fbce01542fdc8fd7a6e57f4
dgwoo01 <dgwoo01@hotmail.com>
2025-07-10 13:38:27 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 set -e
 
 echo "📦 Running 'uv sync'..."
-#uv sync
+/root/.local/bin/uv sync
 
 echo "🚀 Starting server with uvicorn..."
-#exec .venv/bin/uvicorn main:app --host 0.0.0.0 --port 8080
-exec "bash"
+exec .venv/bin/uvicorn main:app --host 0.0.0.0 --port 8080
