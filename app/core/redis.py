fffecfcced067032b0092379d3d89f0f6aae9e38
dgwoo01 <dgwoo01@hotmail.com>
2025-08-31 11:41:32 +0800

    update

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,4 +1,12 @@
 import redis.asyncio as redis
+from typing import AsyncGenerator
 
-async def get_redis(url: str) -> redis.Redis:
-    return redis.from_url(url, decode_responses=True)
+
+async def get_redis(url: str) -> AsyncGenerator[redis.Redis, None]:
+    client = redis.from_url(url, decode_responses=True)
+    try:
+        print("✅ Redis connected")
+        yield client
+    finally:
+        await client.close()
+        print("🔌 Redis disconnected")
