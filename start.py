d9c9652c8067f314bde2c60f3bd3f2c4b6536a63
dgwoo01 <dgwoo01@hotmail.com>
2025-09-04 16:27:39 +0800

    update

--- start.py
+++ start.py
@@ -1,5 +1,14 @@
+import os
 import uvicorn
 
 # uvicorn app.main:app --port=8081
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, headers=[("Server", "AppServer")])
+    # 실행 전 uploads 폴더 생성
+    os.makedirs("uploads", exist_ok=True)
+
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8080,
+        headers=[("Server", "AppServer")],
+    )
